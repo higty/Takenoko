eb170814a30047698216abdf9d1907536259efb5
higty <higuchihiguchihiguchi@hotmail.com>
2020-07-22 16:29:05 +0900

    CSVファイルの作成。

---
+++ Chapter_0013/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Chapter_0013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EncodingProvider provider = System.Text.CodePagesEncodingProvider.Instance;
+            var encoding = provider.GetEncoding(932);
+
+            var body = "名前,年齢,性別,学部,役職";
+            body = body + Environment.NewLine + "新井,19,女,文学部,なし";
+
+            System.IO.File.WriteAllText("C:\\Data\\NewMemberList.csv", body, encoding);
+            Console.WriteLine("保存しました！");
+
+            Console.WriteLine("Enterキーで終了します。");
+            Console.ReadLine();
+        }
+    }
+}
