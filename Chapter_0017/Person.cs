35c046d412d47a87f204ce2cfea840eaf316839e
higty <higuchihiguchihiguchi@hotmail.com>
2020-08-12 17:50:34 +0900

    Add project for property.

---
+++ Chapter_0017/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter_0017
+{
+    public class Person
+    {
+        private String _Name = "名前無し";
+        private Int32 _Age;
+
+        public String Name
+        {
+            get { return _Name; }
+            set
+            {
+                if (value.Length > 10)
+                {
+                    _Name = value.Substring(0, 10);
+                }
+                else
+                {
+                    _Name = value;
+                }
+            }
+        }
+        public Int32 Age
+        {
+            get
+            {
+                return _Age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("value is invalid. value must be greater than or equal 0.");
+                }
+                _Age = value;
+            }
+        }
+        public String OrganizationName { get; set; }
+
+        public String CreateText()
+        {
+            return this.Name + ": " + this.Age + "才 (" + this.OrganizationName + ")";
+        }
+    }
+}
---
+++ Chapter_0017/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Chapter_0017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.Name = "鈴木";
+            p.Age = 23;
+            p.OrganizationName = "営業部";
+            var text = p.CreateText();
+
+            Console.WriteLine(text);
+
+            Console.WriteLine("EnterKeyを入力");
+            Console.ReadLine();
+        }
+    }
+}
