4dee8871f2cef0afa8daea18c54ee18cee6d8a14
higty <higuchihiguchihiguchi@hotmail.com>
2020-08-12 18:46:48 +0900

    Add constructor project.

---
+++ Chapter_0018/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter_0018
+{
+    public class Person
+    {
+        private String _Name = "";
+        private Int32 _Age = 0;
+
+        public String Name
+        {
+            get { return _Name; }
+            set
+            {
+                if (value.Length > 10)
+                {
+                    _Name = value.Substring(0, 10);
+                }
+                else
+                {
+                    _Name = value;
+                }
+            }
+        }
+        public Int32 Age
+        {
+            get { return _Age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                _Age = value;
+            }
+        }
+
+        public Person(String name, Int32 age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public String CreateText()
+        {
+            return this.Name + ": " + this.Age + "才";
+        }
+    }
+}
---
+++ Chapter_0018/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Chapter_0018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person("田中0123456789", -10);
+
+            var text = p.CreateText();
+            Console.WriteLine(text);
+
+            Console.WriteLine("Enter Keyで終了");
+            Console.ReadLine();
+
+        }
+    }
+}
