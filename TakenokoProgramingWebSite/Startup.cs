fe6a417f31d19fe226b43d539e39d3361a9901c9
higty <higuchihiguchihiguchi@hotmail.com>
2020-08-26 14:51:39 +0900

    Add MVC framework to TakenokoProgramingWebSite.

--- TakenokoProgramingWebSite/Startup.cs
+++ TakenokoProgramingWebSite/Startup.cs
@@ -16,6 +16,7 @@ namespace TakenokoProgramingWebSite
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,7 @@ namespace TakenokoProgramingWebSite
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("<div style=\"color:#cc0099;font-family:century gothic;font-size:72px;font-weight:bold;\">Hello Mari!!!!!!</div>");
-                });
+                endpoints.MapControllers();
             });
         }
     }
