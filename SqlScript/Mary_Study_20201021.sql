c45f04f723e790cd93d90bfe5f07f7a3e1beb195
higty <higuchihiguchihiguchi@hotmail.com>
2020-10-21 13:37:26 +0900

    Add study file.

---
+++ SqlScript/Mary_Study_20201021.sql
@@ -0,0 +1,7 @@
+
+
+
+
+
+insert into DBook values ('Harry Potter and the Philosophers Stone','J.K.Rowling',1158,'1997/06/26')
+insert into DBook values ('Alex Rider Storm Breaker','Anthony Horowitz',1513,'2000/09/04')
