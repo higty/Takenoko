b7f98bdf039fb4c5c6867776771963aff6f887d9
higty <higuchihiguchihiguchi@hotmail.com>
2020-12-16 16:21:22 +0900

    Add image to button.

---
+++ TakenokoMusicPlayer/ConfigData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TakenokoMusicPlayer
+{
+    public class ConfigData
+    {
+        public Double Width { get; set; }
+        public Double Height { get; set; }
+        public String FolderPath { get; set; }
+    }
+}
--- TakenokoMusicPlayer/MainWindow.xaml.cs
+++ TakenokoMusicPlayer/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
+using Newtonsoft.Json;
 
 namespace TakenokoMusicPlayer
 {
@@ -33,13 +34,34 @@ namespace TakenokoMusicPlayer
 
             this.FileListBox.MouseDoubleClick += FileListBox_MouseDoubleClick;
             this.ReloadButton.Click += ReloadButton_Click;
+            this.PreviousButton.Click += PreviousButton_Click;
             this.PlayButton.Click += PlayButton_Click;
-            this.PauseButton.Click += PauseButton_Click;
+            this.NextButton.Click += NextButton_Click;
             this.Player.MediaEnded += Player_MediaEnded;
-
-            this.LoadFileList();
+            this.Closing += MainWindow_Closing;
 
             this.FileListBox.ItemsSource = _FileList;
+
+            this.FolderPathTextbox.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            this.LoadConfigFile();
+            this.LoadFileList();
+        }
+
+        private void LoadConfigFile()
+        {
+            try
+            {
+                var filePath = System.IO.Path.Combine(Environment.CurrentDirectory, "Config.txt");
+                var json = File.ReadAllText(filePath);
+                var config = JsonConvert.DeserializeObject<ConfigData>(json);
+                this.Width = config.Width;
+                this.Height = config.Height;
+                this.FolderPathTextbox.Text = config.FolderPath;
+            }
+            catch
+            {
+
+            }
         }
 
         private void FileListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -55,15 +77,41 @@ namespace TakenokoMusicPlayer
         private void LoadFileList()
         {
             _FileList.Clear();
-            foreach (var filePath in Directory.EnumerateFiles(this.FolderPathTextbox.Text))
+            foreach (var filePath in Directory.EnumerateFiles(this.FolderPathTextbox.Text, "*.mp3"
+                , SearchOption.AllDirectories))
             {
                 _FileList.Add(new MediaFile(filePath));
             }
         }
 
+        private void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mediaFile = this.GetPreviousMediaFile();
+            this.Play(mediaFile);
+        }
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
             var mediaFile = this.FileListBox.SelectedItem as MediaFile;
+            if (mediaFile == null)
+            {
+                MessageBox.Show("曲を選択してください。");
+                return;
+            }
+
+            if (_CurrentMediaFile == null)
+            {
+                this.Play(mediaFile);
+            }
+            else
+            {
+                this.Player.Pause();
+                _CurrentMediaFile = null;
+                this.PlayButtonIcon.Source = new BitmapImage(new Uri("./Icon/PlayIcon.png", UriKind.Relative));
+            }
+        }
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mediaFile = this.GetNextMediaFile();
             this.Play(mediaFile);
         }
         private void Play(MediaFile mediaFile)
@@ -75,27 +123,59 @@ namespace TakenokoMusicPlayer
                 this.Player.Source = new Uri(filePath);
             }
             this.Player.Play();
+            this.FileListBox.SelectedItem = mediaFile;
             _CurrentMediaFile = mediaFile;
+            this.PlayButtonIcon.Source = new BitmapImage(new Uri("./Icon/PauseIcon.png", UriKind.Relative));
         }
 
-        private void PauseButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Player.Pause();
-        }
         private void Player_MediaEnded(object sender, RoutedEventArgs e)
         {
             this.Player.Stop();
 
             if (this.IsContinueCheckBox.IsChecked == true)
             {
-                var index = _FileList.IndexOf(_CurrentMediaFile);
-                if (_FileList.Count == index + 1)
-                {
-                    index = -1;
-                }
-                var mediaFile = _FileList[index + 1];
+                var mediaFile = this.GetNextMediaFile();
                 this.Play(mediaFile);
             }
+            else
+            {
+                _CurrentMediaFile = null;
+            }
+        }
+        private MediaFile GetPreviousMediaFile()
+        {
+            var index = _FileList.IndexOf(_CurrentMediaFile);
+            if (index == 0)
+            {
+                index = _FileList.Count;
+            }
+            var mediaFile = _FileList[index - 1];
+            return mediaFile;
+        }
+        private MediaFile GetNextMediaFile()
+        {
+            var index = _FileList.IndexOf(_CurrentMediaFile);
+            if (_FileList.Count == index + 1)
+            {
+                index = -1;
+            }
+            var mediaFile = _FileList[index + 1];
+            return mediaFile;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var w = this.Width;
+            var h = this.Height;
+
+            var config = new ConfigData();
+            config.Width = this.Width;
+            config.Height = this.Height;
+            config.FolderPath = this.FolderPathTextbox.Text;
+            var json = JsonConvert.SerializeObject(config);
+
+            var filePath = System.IO.Path.Combine(Environment.CurrentDirectory, "Config.txt");
+            File.WriteAllText(filePath, json);
         }
     }
 }
