09ebe042a35923ffe2c290b9f6ec5b39d29cbbb2
higty <higuchihiguchihiguchi@hotmail.com>
2020-12-23 14:32:44 +0900

    Add Chapter_0023.

---
+++ Chapter_0023/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Chapter_0023
+{
+    public class MusicPlayer : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Double _Volume = 0;
+        public Double Volume
+        {
+            get { return _Volume; }
+            set
+            {
+                if (_Volume == value) return;
+                _Volume = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Volume)));
+            }
+        }
+
+    }
+}
