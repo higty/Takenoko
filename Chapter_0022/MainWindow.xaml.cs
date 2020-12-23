46b1cda47e2faf828fda6dc15b7437efe890aa73
higty <higuchihiguchihiguchi@hotmail.com>
2020-12-23 12:46:06 +0900

    Add Chapter_0022 project.

---
+++ Chapter_0022/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chapter_0022
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public TaskRecord Record { get; set; } = new TaskRecord();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this.Record;
+        }
+
+        private void ShowValueButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this.Record.Title);
+        }
+
+        private void IncrementPriorityButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Record.Priority += 1;
+        }
+        private void DecrementPriorityButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Record.Priority -= 1;
+        }
+    }
+}
---
+++ Chapter_0022/TaskRecord.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Chapter_0022
+{
+    public class TaskRecord : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private String _Title = "";
+        private Int32 _Priority = 3;
+
+        public String Title
+        {
+            get { return _Title; }
+            set
+            {
+                if (_Title == value) return;
+                _Title = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Title)));
+            }
+        }
+        public String UserName { get; set; }
+        public String Description { get; set; }
+        public Int32 Priority
+        {
+            get { return _Priority; }
+            set
+            {
+                if (_Priority == value) return;
+                _Priority = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Priority)));
+            }
+        }
+    }
+}
