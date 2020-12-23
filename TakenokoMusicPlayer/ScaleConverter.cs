a130eb27d0356a9f60badeb10d8caaded84d56f1
higty <higuchihiguchihiguchi@hotmail.com>
2020-12-23 16:17:50 +0900

    Add ScaleConverter.

---
+++ TakenokoMusicPlayer/ScaleConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace TakenokoMusicPlayer
+{
+    public class ScaleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var x = (Double)value;
+            return x * 100;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var x = (Double)value;
+            return x / 100;
+        }
+    }
+}
