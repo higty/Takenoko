30f8d2ce1849b40ce81e7e872eca2daed35a2908
higty <higuchihiguchihiguchi@hotmail.com>
2020-12-23 16:31:11 +0900

    Fix ScaleConverter to inherite from MarkupExtension.

--- TakenokoMusicPlayer/ScaleConverter.cs
+++ TakenokoMusicPlayer/ScaleConverter.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace TakenokoMusicPlayer
 {
-    public class ScaleConverter : IValueConverter
+    public class ScaleConverter : MarkupExtension, IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var x = (Double)value;
-            return x * 100;
+            return (Int32)(x * 100);
         }
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var x = (Double)value;
             return x / 100;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
         }
     }
 }
