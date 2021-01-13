b1d3b91505e7384cce12528a334083e8ba53ea57
higty <higuchihiguchihiguchi@hotmail.com>
2021-01-13 16:12:08 +0900

    Fix /Signup page.

--- WebAppMySns/Startup.cs
+++ WebAppMySns/Startup.cs
@@ -16,7 +16,11 @@ namespace WebAppMySns
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+#if DEBUG
+            services.AddMvc().AddRazorRuntimeCompilation();
+#else
             services.AddMvc();
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
